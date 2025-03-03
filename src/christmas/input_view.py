620eadcd3640743735c4a5703e31a75b6117e220
AI-WonYJ <yjason7587@naver.com>
2025-03-03 21:56:09 +0900

    feat: Add verification function for entering menu and quantity to order
    주문할 메뉴 및 개수 입력에 대한 검증 기능 추가
    예외처리 경우
     - 주문 형식이 잘못된 경우
     - 수량을 잘못 입력할 경우
     - 메뉴판에 없는 주문일 경우
     - 중복되는 주문일 경우
     - 잘못된 수량일 경우
     - 음료만 주문할 경우
     - 메뉴 개수가 20개가 넘어갈 경우

--- src/christmas/input_view.py
+++ src/christmas/input_view.py
@@ -15,6 +15,70 @@ class InputView:
         while True:
             try:
                 order_input = input("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n")
-                return order_input
-            except ValueError:
-                print("[ERROR]")
+                return InputView.validate_order(order_input)
+            except ValueError as e:
+                print(e)
+
+    @staticmethod
+    def validate_order(order_input):
+        menu = {
+            "양송이수프": 6000, "타파스": 5500, "시저샐러드": 8000,
+            "티본스테이크": 55000, "바비큐립": 54000, "해산물파스타": 35000, "크리스마스파스타": 25000,
+            "초코케이크": 15000, "아이스크림": 5000,
+            "제로콜라": 3000, "레드와인": 60000, "샴페인": 25000
+        }
+        items = order_input.split(",")
+        order = {}
+
+        for item in items:
+            InputView.validate_item_format(item)
+            name, quantity = item.split("-")
+
+            InputView.validate_numeric_quantity(name, quantity)
+            quantity = int(quantity)
+
+            InputView.validate_menu_exists(name, menu)
+            InputView.validate_no_duplicates(name, order)
+            InputView.validate_quantity(name, quantity)
+
+            order[name] = quantity
+
+        InputView.validate_not_only_drinks(order)
+        InputView.validate_max_order_count(order)
+
+        return order
+
+    @staticmethod
+    def validate_item_format(item):
+        if "-" not in item:
+            raise ValueError("[ERROR] 유효하지 않은 주문 형식입니다. 다시 입력해 주세요.")
+
+    @staticmethod
+    def validate_numeric_quantity(name, quantity):
+        if not quantity.isdigit():
+            raise ValueError(f"[ERROR] 잘못된 수량 입력입니다: {name} ({quantity}). 숫자로 입력해 주세요.")
+
+    @staticmethod
+    def validate_menu_exists(name, menu):
+        if name not in menu:
+            raise ValueError(f"[ERROR] 메뉴판에 없는 주문입니다: {name}. 다시 입력해 주세요.")
+
+    @staticmethod
+    def validate_no_duplicates(name, order):
+        if name in order :
+            raise ValueError(f"[ERROR] 중복되는 주문입니다.: {name} 다시 입력해 주세요.")
+
+    @staticmethod
+    def validate_quantity(name, quantity):
+        if quantity < 1:
+            raise ValueError(f"[ERROR] 잘못된 수량입니다: {name} ({quantity}). 1 이상의 숫자를 입력해 주세요.")
+
+    @staticmethod
+    def validate_not_only_drinks(order):
+        if all(k in ["제로콜라", "레드와인", "샴페인"] for k in order):
+            raise ValueError("[ERROR] 음료만 주문할 수 없습니다. 다시 입력해 주세요.")
+
+    @staticmethod
+    def validate_max_order_count(order):
+        if sum(order.values()) > 20:
+            raise ValueError("[ERROR] 최대 20개까지만 주문할 수 있습니다. 다시 입력해 주세요.")
