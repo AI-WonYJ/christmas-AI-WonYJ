9b02bcb45227b77d813960c95f6b07f0756c488c
AI-WonYJ <yjason7587@naver.com>
2025-03-03 21:05:09 +0900

    feat: Add function to input menu items and quantities to order
    주문할 메뉴 및 개수 단순 입력 기능 추가

--- src/christmas/input_view.py
+++ src/christmas/input_view.py
@@ -1,4 +1,5 @@
 class InputView:
+    @staticmethod
     def read_date():
         while True:
             try:
@@ -8,3 +9,12 @@ class InputView:
                 raise ValueError
             except ValueError:
                 print("[ERROR] 유호하지 않은 날짜입니다. 다시 입력해 주세요.")
+
+    @staticmethod
+    def read_order():
+        while True:
+            try:
+                order_input = input("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n")
+                return order_input
+            except ValueError:
+                print("[ERROR]")
