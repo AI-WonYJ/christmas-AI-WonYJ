3cd19d76fbf8963d2617dbbf3092d49e21404222
AI-WonYJ <yjason7587@naver.com>
2025-03-03 20:53:25 +0900

    feat: Add ability to enter visit date
    방문 날짜 입력 기능 추가
    입력 날짜 검증 기능 추가
    잘못된 입력일 경우 재입력 기능 추가

--- src/christmas/main.py
+++ src/christmas/main.py
@@ -1,6 +1,10 @@
+from input_view import InputView
+
 def main():
-    # TODO: 프로그램 구현
-    pass
+    print("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.")
+
+    date = InputView.read_date()
+    print(date)
 
 if __name__ == "__main__":
     main()
