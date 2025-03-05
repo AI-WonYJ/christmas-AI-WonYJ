8b29829ab21c9617967823e5f824ac5a1e2656a9
AI-WonYJ <yjason7587@naver.com>
2025-03-05 20:40:50 +0900

    feat: Add Calculation function excluding discount
    할인과 무관한 계산 기능 추가

--- src/christmas/main.py
+++ src/christmas/main.py
@@ -1,5 +1,6 @@
 from input_view import InputView
 from output_view import OutputView
+from event_planner import EventPlanner
 
 def main():
     print("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.")
@@ -7,7 +8,10 @@ def main():
     date = InputView.read_date()
     order = InputView.read_order()
 
-    OutputView.print_event_summary(date, order)
+    planner = EventPlanner(date, order)
+    planner.calculate_total_price()
+
+    OutputView.print_event_summary(planner)
 
 if __name__ == "__main__":
     main()
